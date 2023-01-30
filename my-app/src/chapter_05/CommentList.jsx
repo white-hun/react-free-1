f5d7f461a40732b44e1b308186f3cba0f4921502
white-hun <121800818+white-hun@users.noreply.github.com>
2023-01-30 14:08:29 +0900

    Update react free

---
+++ my-app/src/chapter_05/CommentList.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Comment from "./comment";
+
+const comments = [
+  {
+    name: "white",
+    comment: "Hello, I'm white",
+  },
+  {
+    name: "Karina",
+    comment: "Keep up the good work",
+  },
+  {
+    name: "Sana",
+    comment: "You did great",
+  },
+];
+
+function CommentList(props) {
+  return (
+    <div>
+      {comments.map((comment) => {
+        return <Comment name={comment.name} comment={comment.comment} />;
+      })}
+    </div>
+  );
+}
+
+export default CommentList;
--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,13 +5,17 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 import Library from "./chapter_03/Library";
+import Clock from "./chapter_04/clock";
+import CommentList from "./chapter_05/CommentList";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <Library />
-  </React.StrictMode>
-);
+setInterval(() => {
+  root.render(
+    <React.StrictMode>
+      <CommentList />
+    </React.StrictMode>
+  );
+}, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
