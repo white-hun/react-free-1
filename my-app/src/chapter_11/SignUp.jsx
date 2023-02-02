7a31e27801f5101efdaba8a86902b44013604be7
white-hun <121800818+white-hun@users.noreply.github.com>
2023-02-02 23:26:07 +0900

    Update react free

--- my-app/src/chapter_11/SignUp.jsx
+++ my-app/src/chapter_11/SignUp.jsx
@@ -38,7 +38,7 @@ function SignUp(props) {
 
 export default SignUp;
 
-// ---------------------------------------------------
+// ---------------------------------------------------시작
  function BoilingVerdict(props) {
     if (props.celsius >= 100) {
         return <p>Boil</p>
@@ -72,7 +72,7 @@ export default Calculator;
 
 const scaleNames = {
     c: "celsius",
-    f: "far"
+    f: "fahrenheit"
 };
 
 function Temperature(props) {
@@ -92,3 +92,110 @@ function Temperature(props) {
     )
 }
 export default Temperature;
+
+// ---------------------------------------------------
+
+function Calculator(props) {
+  return (
+    <div>
+      <TemperatureInput scale="C" />
+      <TemperatureInput scale="F" />
+    </div>
+  );
+}
+
+// ---------------------------------------------------
+function toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+function toFahrenheit(celsius) {
+    return (celsius * 9 / 5)  + 32;
+}
+
+// ---------------------------------------------------
+
+function tryConvert(temperature, tryConvert) {
+    const input = parseFloat(temperature);
+    if (Number.isNaN(input)) {
+        return "";
+    }
+    const output = convert(input);
+    const reounded = Math.round(output * 1000) / 1000;
+    return reounded.toString();
+}
+
+export default tryConvert;
+
+// ---------------------------------------------------
+
+tryConvert("abc", toCelsius);      // empty string을 return
+tryConvert("10.22", toFahrenheit); // '50.396'을 return
+
+// ---------------------------------------------------
+
+return(
+  ...
+  // 변경 전: <input value={temperature} onChange={handleChange} />
+  <input value={props.temperature} onChange={handleChange} />
+  ...
+)
+
+// ---------------------------------------------------
+
+const handleChange = (event) => {
+    // 변경 전 setTemperature(event.target.value);
+    props.onTemperatureChange(event.target.value);
+}
+
+// ---------------------------------------------------
+
+function TemperatureInput(props) {
+    const handleChange = (event) => {
+        props.onTemperatureChange(event.target.value);
+    }
+
+    return (
+      <fieldset>
+          <legend>
+              Write a temperature(level:{scaleNames[props.scale]});
+          </legend>
+              <input value={props.temperature} onChange={handleChange} />
+      </fieldset>
+    )
+}
+
+// ---------------------------------------------------
+
+function Calculator(props)  {
+    const [temperature, setTemperature] = useState("");
+    const [scale, setScale] = useState("C");
+
+    const handleCelsiusChange = (temperature) => {
+      setTemperature(temperature);
+      setScale("c");
+    }
+
+    const handleFahrenheitChange = (temperature) => {
+      setTemperature(temperature);
+      setScale("F");
+    }
+
+    const celsius = scale === "F" ? tryConvert(temperature, toCelsius) : temperature;
+    const fahrenheit = scale === "C" ? tryConvert(temperature, toFahrenheit) : temperature;
+
+    return (
+        <div>
+            <TemperatureInput
+                scale="C"
+                temperature={celsius}
+                onTemperatureChange={handleCelsiusChange} />
+            <TemperatureInput
+                scale="F"
+                temperature={fahrenheit}
+                onTemperatureChange={handleFahrenheitChange} />
+            <BoilingVerdict
+                celsius={parseFloat(celsius)} />
+        </div>
+    )
+}
