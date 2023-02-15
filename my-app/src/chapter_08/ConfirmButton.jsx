b5be0218af53aa6f8ce8f2ca3e7ff6f40416b45a
white-hun <121800818+white-hun@users.noreply.github.com>
2023-02-15 16:47:09 +0900

    Update ConfirmButton.jsx

--- my-app/src/chapter_08/ConfirmButton.jsx
+++ my-app/src/chapter_08/ConfirmButton.jsx
@@ -4,6 +4,13 @@ import React, { useState } from "react";
 function ConfirmButton(props) {
   const [isConfirmed, setIsConfirmed] = useState(false);
 
+  // setState 함수에는 set하고 싶은 값을 직접 넣거나
+  // 새로운 값을 리턴하는 함수를 넣을 수 있다
+  // 함수를 넣게 되면 함수 파라미터로 바로 직전의 state가 전달된다
+  // 바로 직전의 state인 prevIsConfirmed의 값을 받아서 boolean 값을 반전 시켜 return
+  // 함수를 넣는 이유는 확실하게 이전 값을 가지고 변경을 한 뒤에 return하기 위해서이다
+  // 곧바로 값을 넣어서 state를 변경하는 경우 짧은 시간에 빠르게 state가 여러번 변경될 때
+  // 이전 값을 보장하지 못하기 때문이다
   const handleConfirm = () => {
     setIsConfirmed((prevIsConfirmed) => !prevIsConfirmed);
   };
